98c96283015fb6872c69f238c1338482577519bc
Shabeenashabu <102009729+Shabeenashabu@users.noreply.github.com>
2022-03-21 11:31:10 +0530

    Add files via upload

---
+++ ArrayList5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.ArrayList;
+class ArrayList5{
+public static void main(String[] args){
+ArrayList<Integer> h=new ArrayList<Integer>();
+h.add(12);
+h.add(13);
+h.add(14);
+h.add(15);
+h.add(16);
+h.add(17);
+System.out.println(h);
+}
+}
